aee1468cd5f6382583f8f3f9ac5727365d981036
hugoMGSung <personar95@naver.com>
2023-03-08 11:16:20 +0900

    네이버뉴스앱 제작완료

--- part1/studyPyQt/NaverApi.py
+++ part1/studyPyQt/NaverApi.py
@@ -7,7 +7,7 @@ import json # 결과는 json으로
 class NaverApi:
     # 생성자
     def __init__(self) -> None:
-        print('Naver API 생성')
+        print(f'[{datetime.datetime.now()}] Naver API 생성')
 
     # Naver API를 요청함수
     def get_request_url(self, url):
@@ -40,18 +40,4 @@ class NaverApi:
         if retData == None:
             return None
         else:
-            return json.loads(retData) # json으로 return.
-
-
-    # json 데이터 --> list로 변환
-    def get_post_data(self, post, outputs):
-        title = post['title'] 
-        description = post['description']
-        originallink = post['originallink']
-        link = post['link']
-
-        # 'Tue, 07 Mar 2023 17:04:00 +0900' 문자열로 들어온걸 날짜형으로 변경
-        pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
-        pubDate = pDate.strftime('%Y-%m-%d %H:%M:%S') # 2023-03-07 17:04:00 변경
-
-        # outputs에 옮기기 - TO BE CONTINUED...
+            return json.loads(retData) # json으로 return
