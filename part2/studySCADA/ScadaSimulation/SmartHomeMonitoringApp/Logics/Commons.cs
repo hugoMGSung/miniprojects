d046d1277df88bf68a1ea531656e81252cb57d90
hugoMGSung <personar95@naver.com>
2023-05-11 12:32:50 +0900

    5월 11일 커밋

--- part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/Logics/Commons.cs
+++ part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/Logics/Commons.cs
@@ -1,4 +1,8 @@
-﻿using uPLibrary.Networking.M2Mqtt;
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Threading.Tasks;
+using System.Windows;
+using uPLibrary.Networking.M2Mqtt;
 
 namespace SmartHomeMonitoringApp.Logics
 {
@@ -17,5 +21,12 @@ namespace SmartHomeMonitoringApp.Logics
 
         // MQTT 클라이언트 공용 객체
         public static MqttClient MQTT_CLIENT { get; set; }
+
+        // UserControl같이 자식 클래스면서 MetroWindow를 직접사용하지 않아, MahApps.Metro에 있는 Metro메시지창을 못쓸때
+        public static async Task<MessageDialogResult> ShowCustomMessageAsync(string title, string message,
+            MessageDialogStyle style = MessageDialogStyle.Affirmative)
+        {
+            return await ((MetroWindow)Application.Current.MainWindow).ShowMessageAsync(title, message, style, null);
+        }
     }
 }
--- part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/Views/DataBaseControl.xaml.cs
+++ part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/Views/DataBaseControl.xaml.cs
@@ -32,6 +32,10 @@ namespace SmartHomeMonitoringApp.Views
         public bool IsConnected { get; set; }
         Thread MqttThread { get; set; }  // 없으면 UI컨트롤이 어려워짐
 
+        // MQTT Subscribition text 과도문제 속도저하를 잡기위해 변수 
+        // 23.05.11 09:29 SMG
+        int MaxCount { get; set; } = 10;
+
         public DataBaseControl()
         {
             InitializeComponent();
@@ -119,8 +123,17 @@ namespace SmartHomeMonitoringApp.Views
         {
             // 예외처리 필요!!
             this.Invoke(() => {
+                if (MaxCount <= 0)
+                {
+                    TxtLog.Text = string.Empty;
+                    TxtLog.Text += ">>> 문서건수가 많아져 초기화!\n";
+                    TxtLog.ScrollToEnd();
+                    MaxCount = 10;  // 테스트할땐 10, 운영시는 50
+                }
+
                 TxtLog.Text += $"{msg}\n";
                 TxtLog.ScrollToEnd();
+                MaxCount--;
             });
         }
 
